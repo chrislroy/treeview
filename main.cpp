f0b4bfa529ab4a94437184bab3e43019726e4828
Christian L. Roy <christian.l.roy@autodesk.com>
2018-09-18 09:14:30 -0400

    new style sheet

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 
 
 #include <QtWidgets/QApplication>
+#include <QFile>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication qapp(argc, argv);
+
+    QFile File("stylesheet.qss");
+    File.open(QFile::ReadOnly);
+    QString StyleSheet = QLatin1String(File.readAll());
+
+    qapp.setStyleSheet(StyleSheet);
     MainWindow w;
     w.show();
-    return a.exec();
+    return qapp.exec();
 }
